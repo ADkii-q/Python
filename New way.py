93228a7b1d1c148798a4e365b1d84540151f38cf
Mark <70965699+ADkii-q@users.noreply.github.com>
2021-10-09 19:45:57 +0300

    Update New way.py

--- New way.py
+++ New way.py
@@ -1,7 +1,6 @@
 ================( Реализовать кусочно линейную функцию )=============================
 import math
 
-
 def f12(x):
     if x < 144:
         return result1(x)
@@ -12,23 +11,14 @@ def f12(x):
     if x >= 234:
         return result4(x)
 
-
 def result1(x):
     return pow(x, 3) - math.log10(x)
-
-
 def result2(x):
     return pow(x, 8) - math.log10(x)
-
-
 def result3(x):
     return (pow(x, 5) / 56) + math.cos(x) - 28
-
-
 def result4(x):
     return 84 * x - math.sin(x) + 56
-
-
 print(f12(91))
 
 ================(  )=============================
