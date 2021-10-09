4d601c9d3b4755fa47eed6450ec1662bf8df2046
Mark <70965699+ADkii-q@users.noreply.github.com>
2021-10-09 19:36:22 +0300

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,11 +1,7 @@
 import math
 
 def task11(x, y):
-     """
-    Calculates and prints result of mathematical operations with two parameters
-    :param x: First parameter
-    :param y: Second parameter
-    """
+
     a = math.pow(x, 3) - 23 * math.pow(y, 4)
     b = math.pow(y, 5) + math.log(y)
     c = (math.pow(y, 2) / 56) + math.cos(y) - 28
@@ -19,10 +15,7 @@ print (task11 (x, y))
 ####################################################################################
 
 def task12(x):
-     """
-    Calculates result of mathematical operations with one parameter depending on the specific conditions
-    :param x: Parameter with which to operate
-    """
+
     if x < 144:
         print(f"{function1(x):.2e}")
 
@@ -36,35 +29,15 @@ def task12(x):
         print(f"{function4(x):.2e}")
 
 def function1(x):
-     """
-    Returns result of mathematical operations with one parameter
-    :param x: Parameter with which to operate
-    :return: Result of mathematical operations with one parameter
-    """
 	return math.pow(x, 3) - math.log(x)
 
 def function2(x):
-     """
-    Returns result of mathematical operations with one parameter
-    :param x: Parameter with which to operate
-    :return: Result of mathematical operations with one parameter
-    """
 	return math.pow(x, 8) - math.log(x)
 
 def function3(x):
-    """
-    Returns result of mathematical operations with one parameter
-    :param x: Parameter with which to operate
-    :return: Result of mathematical operations with one parameter
-    """
 	return math.pow(x, 5) / 56 + math.cos(x) - 28
 
 def function4(x):
-    """
-    Returns result of mathematical operations with one parameter
-    :param x: Parameter with which to operate
-    :return: Result of mathematical operations with one parameter
-    """
 	return 84 * x - math.sin(x) + 56
 
 
