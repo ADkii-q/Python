2f31c10b18ec172deb9bf507f2e0ffff56ebbed8
Mark <70965699+ADkii-q@users.noreply.github.com>
2021-10-10 17:36:06 +0300

    Update New way.py

--- New way.py
+++ New way.py
@@ -21,4 +21,23 @@ def result4(x):
     return 84 * x - math.sin(x) + 56
 print(f12(91))
 
-================(  )=============================
+================( Реализовать итерационную функцию )=================================
+import math
+
+def f13(n, m) -> float:
+    return sum1(1, 1, n, m) + sum2(1, n)
+
+def sum1(i, j, n, m) -> float:
+    result = 0.0
+    for x in range(i, n + 1):
+        for y in range(j, m + 1):
+            result = result + (pow(x, 3) - 23 * pow(y, 4))
+    return result
+
+def sum2(i, n) -> float:
+    result = 0.0
+    for x in range(i, n + 1):
+        result = result + (math.fabs(x) + math.cos(x))
+    return result
+
+print(f13(91, 23))
