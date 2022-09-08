7af3db7e48367bbe640226b12a11f1a65d6eb9b2
Mark <70965699+ADkii-q@users.noreply.github.com>
2022-09-08 19:21:42 +0300

    Update Pract1.py

--- Big Data/Pract1.py
+++ Big Data/Pract1.py
@@ -1,1 +1,21 @@
+import math
 
+what = input("Фигуры: треугольник, прямоугольник, круг")
+
+if what == "треугольник":
+    d = float(input("Основание"))
+    h = float(input("Высота"))
+    S = (d * h) / 2
+    print("Результат: " + str(S))
+elif what == "прямоугольник":
+    a = float(input("Сторона а"))
+    b = float(input("Сторона б"))
+    S = a * b
+    print("Результат: " + str(S))
+elif what == "круг":
+    R = float(input("Радиус "))
+    S = math.pi * math.pow(R, 2)
+    print("Результат: " + str(S))
+else:
+    print("Выбрана неверная операция")
+    
